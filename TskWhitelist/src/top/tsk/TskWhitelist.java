ca058595910a73b3693d6a69342c4c211647ae1e
Shukang Tong <tongshukang@caicloud.io>
2019-06-10 00:38:33 +0800

    feat(whitelist): event handler finished

---
+++ TskWhitelist/src/top/tsk/TskWhitelist.java
@@ -0,0 +1,112 @@
+package top.tsk;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class TskWhitelist extends JavaPlugin implements Listener {
+
+    static final String CONFIG_REFUSE = "refuse-message";
+
+    static final String CONFIG_WHITE = "white-list";
+    static final String CONFIG_BLACK = "black-list";
+    static final String CONFIG_GRAY = "gray-list";
+
+    static final String CONFIG_ENABLE = "enabled";
+
+
+    String refuseMessage;
+
+    List whiteList;
+    List blackList;
+    List grayList;
+
+    Boolean enable;
+
+    public void initConfig() {
+        getConfig().set(CONFIG_REFUSE, "You are not in whitelist");
+
+        getConfig().set(CONFIG_WHITE, new LinkedList<String>());
+        getConfig().set(CONFIG_BLACK, new LinkedList<String>());
+        getConfig().set(CONFIG_GRAY, new LinkedList<String>());
+
+        getConfig().set(CONFIG_ENABLE, false);
+    }
+
+    @Override
+    public void onLoad() {
+        if (!new File(getDataFolder(), "config.yml").exists()) {
+            saveDefaultConfig();
+            initConfig();
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+
+
+        refuseMessage = getConfig().getString(CONFIG_REFUSE);
+
+        whiteList = getConfig().getStringList(CONFIG_WHITE);
+        blackList = getConfig().getStringList(CONFIG_BLACK);
+        grayList = getConfig().getStringList(CONFIG_GRAY);
+
+        enable = getConfig().getBoolean(CONFIG_ENABLE);
+    }
+
+    @Override
+    public void onDisable() {
+
+        getConfig().set(CONFIG_REFUSE, refuseMessage);
+
+        getConfig().set(CONFIG_WHITE, whiteList);
+        getConfig().set(CONFIG_BLACK, blackList);
+        getConfig().set(CONFIG_GRAY, grayList);
+
+        getConfig().set(CONFIG_ENABLE, enable);
+
+
+        saveConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        // TODO
+        return super.onCommand(sender, command, label, args);
+    }
+
+    @EventHandler
+    public void preLogin(AsyncPlayerPreLoginEvent event) {
+
+        if (enable) {
+            if (whiteList.contains(event.getName())) {
+                event.allow();
+            } else {
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, refuseMessage);
+
+                if (!blackList.contains(event.getName())) {
+                    if (!grayList.contains(event.getName())) {
+                        grayList.add(event.getName());
+                    }
+                }
+            }
+        } else {
+            if (blackList.contains(event.getName())) {
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, refuseMessage);
+            } else {
+                event.allow();
+            }
+        }
+    }
+}
+
