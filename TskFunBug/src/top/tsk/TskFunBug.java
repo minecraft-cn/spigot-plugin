39679e3492759e9359fbdd8a827a6a848de7b44e
tskdsb <tsk912@qq.com>
2019-07-10 21:51:01 +0800

    feat(TskFunBug): over potion max-level; over enchant max-level

--- TskFunBug/src/top/tsk/TskFunBug.java
+++ TskFunBug/src/top/tsk/TskFunBug.java
@@ -2,20 +2,22 @@ package top.tsk;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -23,18 +25,20 @@ import top.tsk.utils.TskUtils;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class TskFunBug extends JavaPlugin implements Listener {
-
   public boolean CreeperNoBlock = true;
   public boolean ShowExp = true;
   public boolean RideAny = true;
-  public boolean LeashAny = true;
-  public boolean TransferBuffToMonster = true;
+  public boolean LeashAny = false;
+  public boolean TransferBuffToNonPlayer = true;
   public boolean HatEverything = true;
-  public boolean DropPotionOnDeath = true;
+  public boolean DropPotionWhenKilledByPlayer = true;
   public boolean DropEquipmentForPoorPlayer = true;
-
+  public boolean OverMaxEnchLevelToX = true;
 
   @Override
   public void onLoad() {
@@ -96,35 +100,84 @@ public class TskFunBug extends JavaPlugin implements Listener {
     if (TskUtils.IsItemEmpty(itemMainHand)) {
       return;
     }
-    if (itemMainHand.getType().equals(Material.LEAD)) {
-      if (e.getRightClicked() instanceof LivingEntity) {
-        e.setCancelled(true);
-        ((LivingEntity) e.getRightClicked()).setLeashHolder(e.getPlayer());
-        itemMainHand.setAmount(itemMainHand.getAmount() - 1);
-        e.getPlayer().getEquipment().setItemInMainHand(itemMainHand);
-      }
+    if (itemMainHand.getType().equals(Material.LEAD) &&
+      e.getRightClicked() instanceof LivingEntity
+    ) {
+      e.setCancelled(true);
+      ((LivingEntity) e.getRightClicked()).setLeashHolder(e.getPlayer());
+      itemMainHand.setAmount(itemMainHand.getAmount() - 1);
+      e.getPlayer().getEquipment().setItemInMainHand(itemMainHand);
     }
   }
 
-  @EventHandler
-  public void TransferBuffToMonster(PlayerDeathEvent event) {
-    if (!TransferBuffToMonster) {
-      return;
-    }
-    Player player = event.getEntity();
-    if (player.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
-      Entity damager = ((EntityDamageByEntityEvent) player.getLastDamageCause()).getDamager();
-      if (damager instanceof LivingEntity) {
-        if (player.getActivePotionEffects().isEmpty()) {
-          return;
-        }
-        Collection<PotionEffect> potionEffects = player.getActivePotionEffects();
-        ((LivingEntity) damager).addPotionEffects(potionEffects);
-        ((LivingEntity) damager).setRemoveWhenFarAway(false);
-        for (PotionEffect potionEffect : potionEffects) {
-          player.removePotionEffect(potionEffect.getType());
+  Collection<PotionEffect> combinePotion(Collection<PotionEffect> p1, Collection<PotionEffect> p2) {
+    Collection<PotionEffect> result = new LinkedList<>();
+    Iterator<PotionEffect> iterator1 = p1.iterator();
+    Iterator<PotionEffect> iterator2 = p2.iterator();
+    while (iterator1.hasNext()) {
+      PotionEffect potionEffect1 = iterator1.next();
+      while (iterator2.hasNext()) {
+        PotionEffect potionEffect2 = iterator2.next();
+        if (potionEffect2.getType().equals(potionEffect1.getType())) {
+          int amplifier;
+          long duration;
+          if (potionEffect1.getAmplifier() == potionEffect2.getAmplifier()) {
+            amplifier = potionEffect1.getAmplifier() + 1;
+            if (amplifier > 10) {
+              amplifier = 10;
+            }
+            duration = potionEffect1.getDuration() + potionEffect2.getDuration();
+            if (duration > Integer.MAX_VALUE) {
+              duration = Integer.MAX_VALUE;
+            }
+          } else {
+            amplifier = potionEffect1.getAmplifier() > potionEffect2.getAmplifier() ? potionEffect1.getAmplifier() : potionEffect2.getAmplifier();
+            duration = potionEffect1.getDuration() > potionEffect2.getDuration() ? potionEffect1.getDuration() : potionEffect2.getDuration();
+          }
+          result.add(new PotionEffect(potionEffect1.getType(), ((int) duration), amplifier));
+          iterator1.remove();
+          iterator2.remove();
+          break;
         }
       }
+    }
+    result.addAll(p1);
+    result.addAll(p2);
+    return result;
+  }
+
+  @EventHandler
+  public void TransferBuffToNonPlayer(EntityDeathEvent event) {
+    if (!TransferBuffToNonPlayer) {
+      return;
+    }
+    if (event.getEntity().getKiller() != null) {
+      return;
+    }
+    if (event.getEntity().getActivePotionEffects().isEmpty()) {
+      return;
+    }
+    LivingEntity entity = event.getEntity();
+    Entity killer = TskUtils.GetKiller(entity);
+    if (killer == null) {
+      return;
+    }
+    if (!(killer instanceof LivingEntity)) {
+      return;
+    }
+    LivingEntity livingEntity = (LivingEntity) killer;
+    if (livingEntity.getActivePotionEffects().isEmpty()) {
+      livingEntity.addPotionEffects(entity.getActivePotionEffects());
+    } else {
+      Collection<PotionEffect> potionEffects = combinePotion(entity.getActivePotionEffects(), livingEntity.getActivePotionEffects());
+      for (PotionEffect potionEffect : livingEntity.getActivePotionEffects()) {
+        livingEntity.removePotionEffect(potionEffect.getType());
+      }
+      livingEntity.addPotionEffects(potionEffects);
+    }
+    livingEntity.setRemoveWhenFarAway(false);
+    for (PotionEffect potionEffect : entity.getActivePotionEffects()) {
+      entity.removePotionEffect(potionEffect.getType());
     }
   }
 
@@ -152,23 +205,30 @@ public class TskFunBug extends JavaPlugin implements Listener {
   }
 
   @EventHandler
-  public void DropPotionOnDeath(EntityDeathEvent event) {
-    if (!DropPotionOnDeath) {
+  public void DropPotionWhenKilledByPlayer(EntityDeathEvent event) {
+    if (!DropPotionWhenKilledByPlayer) {
       return;
     }
-    if (event.getEntity().getKiller() != null) {
-      // 再加一个条件，比如玩家需要有【炼药】附魔或者手执火焰棒
-      // 写到这里，突然想到，需要考虑玩家体验。你说以一种什么样的方式，玩家会玩得比较舒服，而不是喷【这是什么脑残设计？】
-      Collection<PotionEffect> activePotionEffects = event.getEntity().getActivePotionEffects();
-      for (PotionEffect effect : activePotionEffects) {
-        ItemStack itemStack = new ItemStack(Material.POTION);
-        PotionMeta potionMeta = ((PotionMeta) itemStack.getItemMeta());
-        potionMeta.addCustomEffect(effect, true);
-        potionMeta.setColor(effect.getType().getColor());
+    if (event.getEntity() instanceof Player) {
+      return;
+    }
+    if (event.getEntity().getKiller() == null) {
+      return;
+    }
+    if (event.getEntity().getActivePotionEffects().isEmpty()) {
+      return;
+    }
+    // 再加一个条件，比如玩家需要有【炼药】附魔或者手执火焰棒
+    // 写到这里，突然想到，需要考虑玩家体验。你说以一种什么样的方式，玩家会玩得比较舒服，而不是喷【这是什么脑残设计？】
+    Collection<PotionEffect> activePotionEffects = event.getEntity().getActivePotionEffects();
+    for (PotionEffect effect : activePotionEffects) {
+      ItemStack itemStack = new ItemStack(Material.POTION);
+      PotionMeta potionMeta = ((PotionMeta) itemStack.getItemMeta());
+      potionMeta.addCustomEffect(effect, true);
+      potionMeta.setColor(effect.getType().getColor());
 //                potionMeta.setDisplayName(ChatColor.RESET + effect.getType().getName());
-        itemStack.setItemMeta(potionMeta);
-        event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), itemStack);
-      }
+      itemStack.setItemMeta(potionMeta);
+      event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), itemStack);
     }
   }
 
@@ -217,5 +277,114 @@ public class TskFunBug extends JavaPlugin implements Listener {
       event.getDrops().add(monster.getEquipment().getHelmet());
     }
   }
+
+  Collection<PotionEffect> combineEnch(Collection<PotionEffect> p1, Collection<PotionEffect> p2) {
+    Collection<PotionEffect> result = new LinkedList<>();
+    Iterator<PotionEffect> iterator1 = p1.iterator();
+    Iterator<PotionEffect> iterator2 = p2.iterator();
+    while (iterator1.hasNext()) {
+      PotionEffect potionEffect1 = iterator1.next();
+      while (iterator2.hasNext()) {
+        PotionEffect potionEffect2 = iterator2.next();
+        if (potionEffect2.getType().equals(potionEffect1.getType())) {
+          int amplifier;
+          long duration;
+          if (potionEffect1.getAmplifier() == potionEffect2.getAmplifier()) {
+            amplifier = potionEffect1.getAmplifier() + 1;
+            if (amplifier > 10) {
+              amplifier = 10;
+            }
+            duration = potionEffect1.getDuration() + potionEffect2.getDuration();
+            if (duration > Integer.MAX_VALUE) {
+              duration = Integer.MAX_VALUE;
+            }
+          } else {
+            amplifier = potionEffect1.getAmplifier() > potionEffect2.getAmplifier() ? potionEffect1.getAmplifier() : potionEffect2.getAmplifier();
+            duration = potionEffect1.getDuration() > potionEffect2.getDuration() ? potionEffect1.getDuration() : potionEffect2.getDuration();
+          }
+          result.add(new PotionEffect(potionEffect1.getType(), ((int) duration), amplifier));
+          iterator1.remove();
+          iterator2.remove();
+          break;
+        }
+      }
+    }
+    result.addAll(p1);
+    result.addAll(p2);
+    return result;
+  }
+
+  @EventHandler
+  public void OverMaxEnchLevelToX(PrepareAnvilEvent event) {
+    if (!OverMaxEnchLevelToX) {
+      return;
+    }
+    if (TskUtils.IsItemEmpty(event.getResult())) {
+      return;
+    }
+    ItemStack itemStack1 = event.getInventory().getItem(0);
+    ItemStack itemStack2 = event.getInventory().getItem(1);
+    ItemStack result = event.getResult();
+    Map<Enchantment, Integer> enchants1 = TskUtils.GetEnchants(itemStack1);
+    if (enchants1.isEmpty()) {
+      return;
+    }
+    Map<Enchantment, Integer> enchants2 = TskUtils.GetEnchants(itemStack2);
+    if (enchants1.isEmpty()) {
+      return;
+    }
+    Map<Enchantment, Integer> enchants3 = TskUtils.GetEnchants(result);
+
+    int extraCost = 0;
+    for (Map.Entry<Enchantment, Integer> entry : enchants3.entrySet()) {
+      if (entry.getKey().getMaxLevel() == 1) {
+        continue;
+      }
+      if (enchants1.containsKey(entry.getKey()) && enchants2.containsKey(entry.getKey())) {
+        if (enchants1.get(entry.getKey()).equals(enchants2.get(entry.getKey()))) {
+          if (enchants1.get(entry.getKey()) >= entry.getKey().getMaxLevel()) {
+            Integer level = enchants1.get(entry.getKey()) + 1;
+            if (level > 10) {
+              level = 10;
+            }
+            extraCost += level;
+            entry.setValue(level);
+          }
+        } else {
+          Integer level = enchants1.get(entry.getKey()) > enchants2.get(entry.getKey()) ?
+            enchants1.get(entry.getKey()) : enchants2.get(entry.getKey());
+          if (level > entry.getKey().getMaxLevel()) {
+            entry.setValue(level);
+          }
+        }
+      }
+    }
+    ItemMeta resultItemMeta = result.getItemMeta();
+    if (resultItemMeta instanceof EnchantmentStorageMeta) {
+      for (Map.Entry<Enchantment, Integer> entry : enchants3.entrySet()) {
+        ((EnchantmentStorageMeta) resultItemMeta).addStoredEnchant(entry.getKey(), entry.getValue(), true);
+      }
+      result.setItemMeta(resultItemMeta);
+    } else {
+      result.addUnsafeEnchantments(enchants3);
+    }
+    event.setResult(result);
+    event.getInventory().setRepairCost(event.getInventory().getRepairCost() + extraCost);
+    event.getInventory().setMaximumRepairCost(event.getInventory().getMaximumRepairCost() + extraCost);
+  }
+//
+//  String textures = "textures";
+//
+//  String texturesValue = "eyJ0aW1lc3RhbXAiOjE1NjI1MDUzNDY3MzYsInByb2ZpbGVJZCI6IjY3NDg3MTE2ODQxYzQ4MjI4YjJjNWFiMDRmMjBiYTZhIiwicHJvZmlsZU5hbWUiOiJMdXB1c1RleGFzIiwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2U5Yjg0MDkzNTIzYzk1Y2U4YTY1MDMxMGE0MmQ5NDU3ZDNhN2VkOGY3YzNhNTc3MTM0MWUzNjUzOTFhMDg0MTAiLCJtZXRhZGF0YSI6eyJtb2RlbCI6InNsaW0ifX19fQ==";
+//
+//  @EventHandler
+//  public void setSkin(PlayerJoinEvent event) {
+//    System.out.println(event.getPlayer().getAddress());
+//    CraftPlayer craftPlayer = (CraftPlayer) event.getPlayer();
+////    craftPlayer.getProfile().getProperties().put(textures, new Property(textures, texturesValue));
+//    for (Field field : craftPlayer.getClass().getFields()) {
+//      System.out.println(field.getName() + "\t" + field.getType());
+//    }
+//  }
 }
     
